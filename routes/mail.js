cc966a8527ab0b80d0caf35749bf098e481737f8
Juan Rivera <jrivera@primalplatoon.com>
2020-09-23 18:20:18 -0400

    Changed recipient back to Juan until David verifies his email.

--- routes/mail.js
+++ routes/mail.js
@@ -13,7 +13,7 @@ const sendMail = (name, email, message, cb) => {
   // console.log(name, email + "Inside sendMail");
   const mailOptions = {
     from: "juancodeatatime@gmail.com",
-    to: "david.jones@acdmilserve.com",
+    to: "juancodeatatime@gmail.com",
     subject: `Contact form - ${process.env.APP_NAME}`,
     // text: `Email received from Sender name: ${name} \n Sender email: ${email} \n Sender message: ${message}`,
     // text: "test worked!!",
